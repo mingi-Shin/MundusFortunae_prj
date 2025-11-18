39130ae969a38258aa8405cead3b1af9d7f19c96
shinseogyong <108261108+shinseogyong@users.noreply.github.com>
2025-11-18 20:12:18 +0900

    1118

---
+++ MundusFortunae/src/main/java/com/mingisoft/mf/MundusFortunaeApplication.java
@@ -0,0 +1,13 @@
+package com.mingisoft.mf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundusFortunaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundusFortunaeApplication.class, args);
+	}
+
+}
---
+++ MundusFortunae/src/test/java/com/mingisoft/mf/MundusFortunaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingisoft.mf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MundusFortunaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
